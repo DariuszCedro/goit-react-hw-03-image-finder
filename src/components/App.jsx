f311d79d5cc0684d64c65f7d94d798c098395186
DariuszCedro <dariuszcedro@gmail.com>
2024-01-26 18:53:24 +0100

    added component specific folders and .jsx/.css files templates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Modal } from './Modal/Modal';
+import { Loader } from './Loader/Loader';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>React homework template</div>;
 };
